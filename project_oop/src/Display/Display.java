12a67216485bfb8b8c6ab0bada430da6dd5f1151
user <user@Ri>
2022-12-11 12:53:21 +0700

    Start sak tee

---
+++ project_oop/src/Display/Display.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Display;
+
+// JFrame for input other Jpanel
+public class Display {
+    
+}
