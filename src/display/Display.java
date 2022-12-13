9a4f507161b9cddc3a462c12bf1e639b93cdef86
Sanpis Poolsilpe <sanpis1234@gmail.com>
2022-12-13 13:37:53 +0700

    textfield

--- src/display/Display.java
+++ src/display/Display.java
@@ -4,26 +4,35 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import Element.Element;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 public class Display extends JFrame implements ActionListener{
 
 	private static final long serialVersionUID = 1L;
-	private Dimension size = new Dimension(1000,600);
-        private JTextField tf = new JTextField(10);
+	private Dimension size = new Dimension(1000,700);
+        private JPanel p;
+        private JTextField tf;
 		
 	public Display() {
 		this.setting();
-                                    this.add(tf, BorderLayout.SOUTH);
-		this.getContentPane().add(new Game());
+                                    this.add(p, BorderLayout.SOUTH);
+		this.getContentPane().add(new Game(),BorderLayout.CENTER);
+                
 	}
 	
 	private void setting() {
 		this.setTitle("Dog ninja");
 		this.setSize(size);
-                this.setLayout(new BorderLayout());
+                                    this.setLayout(new BorderLayout());
+                                    tf = new JTextField(20);
+                                    tf.setBorder(new EmptyBorder(5,0,5,0));
+                                    tf.setFont(Element.getFont(30));
+                                    p = new JPanel();
+                                    p.add(tf);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocation(280,100);
                                    
