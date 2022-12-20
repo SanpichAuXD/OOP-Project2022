16a48d542560c2b1167d6765e6a0050798dde6a8
Sanpis Poolsilpe <sanpis1234@gmail.com>
2022-12-20 15:31:38 +0700

    clean code ใช้ไม้กวาด

--- src/Charactor/Wave.java
+++ src/Charactor/Wave.java
@@ -1,4 +1,5 @@
 package Charactor;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
@@ -8,23 +9,23 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class Wave {
+
     public int speed;
     public int x;
     public int y;
     public int num = 0;
     public int point;
-    public String vord, path;
+    public String vord;
     Timer timeMove;
     public int upSpeed = 2;
     public int upPoint;
-    
+
     public Wave(int x, int y, int speed, String vord, int point, int c_point, JPanel page) {
         this.x = x;
         this.y = y;
         this.speed = speed;
         this.vord = vord;
         this.point = point;
-        path = "img\\zom0.png";
         this.move(page, c_point);
     }
 
@@ -39,29 +40,10 @@ public class Wave {
                     x -= (10 + upSpeed);
                 }
                 page.repaint();
-                path = svapImage();
             }
         });
         this.timeMove.start();
 
     }
 
-    public BufferedImage getImage() {
-        BufferedImage image = null;
-        try {
-            image = ImageIO.read(new File(path));
-            return image;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return image;
-    }
-
-    public String svapImage() {
-        String path = "img\\zom0.png";
-        int pic = num % 7;
-        path = "img\\zom" + pic + ".png";
-        num += 1;
-        return path;
-    }
 }
